901a71d9db6e506fd46471a9fab6e585b6c9b730
HSSJW <132650844+HSSJW@users.noreply.github.com>
2023-10-23 16:40:29 +0900

    [Silver III] Title: 발머의 피크 이론, Time: 76 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/27496. 발머의 피크 이론/발머의 피크 이론.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n, l;
+	cin >> n >> l;
+	long long sum = 0;
+	int count = 0;
+
+	vector<int> v(n + 1);
+	v[0] = 0;
+
+	for (int i = 1; i < n+1; i++) {
+		cin >> v[i];
+		v[i] = v[i - 1] + v[i];
+
+	}
+	
+	if (n <= l) {
+		for (int i = 1; i < n + 1; i++) {
+			if (129 <= v[i] && v[i] <= 138)
+				count++;
+		}
+	}
+
+	else {
+		
+		for (int i = 1; i < l; i++) {
+			if (129 <= v[i] && v[i] <= 138)
+				count++;
+		}
+
+
+		for (int i = l; i < n + 1; i++) {
+
+			sum = v[i] - v[i - l];
+
+			if (129 <= sum && sum <= 138) {
+				count++;
+			}
+		}
+	}
+	cout << count;
+
+
+	return 0;
+}

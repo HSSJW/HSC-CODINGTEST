f89f17683e7698718b7055797169ff9afc1da26a
HSSJW <132650844+HSSJW@users.noreply.github.com>
2024-01-02 17:01:57 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main() {
+
+	int n;
+	cin >> n;
+	long long arr[91];
+
+	arr[0] = 0;
+	arr[1] = 1;
+
+
+	for (int i = 2; i <= 90; i++) {
+		arr[i] = arr[i - 1] + arr[i - 2];
+	}
+
+
+	cout << arr[n];
+
+
+	return 0;
+}

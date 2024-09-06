20241ccd6a91202144a8fd18653162acbf52119b
HSSJW <132650844+HSSJW@users.noreply.github.com>
2024-09-06 15:57:17 +0900

    [Bronze I] Title: 2007년, Time: 64 ms, Memory: 38044 KB -BaekjoonHub

---
+++ 백준/Bronze/1924. 2007년/2007년.py
@@ -0,0 +1,12 @@
+import sys, datetime
+
+input = sys.stdin.readline
+
+
+x, y = map(int, input().split())
+
+
+days = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
+
+print(days[datetime.date(2007, x, y).weekday()])
+

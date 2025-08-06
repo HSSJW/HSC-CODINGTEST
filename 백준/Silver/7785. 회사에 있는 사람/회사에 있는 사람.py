3be541c864af95a1748dff30cd0cf06f830781c9
Song JinWoo <132650844+HSSJW@users.noreply.github.com>
2025-08-06 15:16:16 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 2620 ms, Memory: 42656 KB -BaekjoonHub

---
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
@@ -0,0 +1,18 @@
+n = int(input())
+
+enterList = set() #출근자 목록
+
+for i in range(n):
+    name, status = input().split(" ")
+
+    if status == "enter":
+        enterList.add(name) # 출근처리
+    else:
+        enterList.discard(name) # 퇴근처리
+
+resultList = sorted(enterList, reverse=True)
+
+for name in resultList:
+    print(name)
+
+

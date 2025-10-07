19247ffaf444834cc9155a7cc913ed7288e5365f
Song JinWoo <132650844+HSSJW@users.noreply.github.com>
2025-10-07 19:50:58 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 9.36 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.py
@@ -0,0 +1,16 @@
+def solution(n):
+    answer = 0
+
+    n = str(n)
+    num_list = list(n)
+    
+    num = [int(n) for n in num_list]
+    
+    for i in num:
+        answer += i
+        
+        
+        
+    
+
+    return answer

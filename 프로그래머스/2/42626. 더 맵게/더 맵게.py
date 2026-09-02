5b0ab834a1fb9480d98b6212e280558c9b8b8490
Song JinWoo <132650844+HSSJW@users.noreply.github.com>
2026-09-02 13:33:33 +0900

    [level 2] Title: 더 맵게, Time: 1685.54 ms, Memory: 52.6 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,42 +1,40 @@
+"""
+<힌트>
+- 모든 음식의 스코빌 지수를 K 이상으로 만들고싶다.
+섞은 음식의 스코빌 지수 = 가장 맵지 않은 음식의 스코빌 지수 + (두 번째로 맵지 않은 음식의 스코빌 지수 * 2)
+
+- 모든 음식의 스코빌 지수가 K 이상일 때까지 '반복' -> 가장 맵, 두번 째 맵 반복 추출
+    -> 힙
+    
+<조건>
+- scoville : 스코빌 지수 리스트
+
+<목표>
+- 섞는 최소 횟수
+
+
+"""
+
 import heapq
 
 def solution(scoville, K):
     answer = 0
     
-    """
-    가장 낮은 두개의 음식을 섞는다
-    가장 낮은 스코빌 + 두번째로 낮은 스코빌*2 -> K이상이 될 때까지 반복
     
-    최소 몇번을 섞어야 하는지
+    heapq.heapify(scoville)
     
-    1. 현재 스코빌지수(how) >= K?
-    2. 안넘는다 -> 섞는다
-    3. how = 가장 낮은 스코빌 + 두번째로 낮은 스코빌*2 / answer += 1
-    4. 1~3 반복
-    
-    불가능하면 -1
-    """
-    
-    if K == 0:
-        answer = 0
-        return answer
-    
-    heapq.heapify(scoville) # 힙으로 변환 -> heap[0]이 최솟값인걸 보장
-
-    
-    while scoville[0] < K: # 최소 스코빌지수(힙의 0번 인덱스(최소값))가 K이상이 될때 까지 반복
-        if len(scoville) != 1:
-            
-            min_scov = heapq.heappop(scoville)
-            sec_scov = heapq.heappop(scoville)
-            
-            heapq.heappush(scoville, min_scov + sec_scov * 2) # 섞기 -> 섞은 음식 제거
-            answer += 1
-            
-        else:
-            answer = -1
-            break
+    while len(scoville) > 1 and scoville[0] < K: # 가장 작은 스코빌지수가 K 이상
         
-    
-    
+        first = heapq.heappop(scoville)
+        second = heapq.heappop(scoville)
+        
+        tmp = first + second * 2 # 섞은 음식
+        
+        heapq.heappush(scoville, tmp)
+        answer += 1
+        
+    if scoville[0] < K:
+        return -1
+        
     return answer
+

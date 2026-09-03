b0874cba9d4bcff47c407ae9f443c0176da60d39
Song JinWoo <132650844+HSSJW@users.noreply.github.com>
2026-09-03 22:17:37 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 202.99 ms, Memory: 11 MB -BaekjoonHub

--- 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.py
+++ 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.py
@@ -1,47 +1,47 @@
 """
-뒤집어도 똑같은 문자열
+<힌트>
+- 팰린드롬 : 앞뒤를 뒤집어도 똑같은 문자열
+- s의 부분문자열 중에서 가장 긴 팰린드롬의 길이
+    -> 대칭성 확인
 
-<목표>
-- 부분 문자열 중 가장 긴 '팰린드롬'의 길이
--> 조건을 만족하는 최소 길이 검사 -> 파나메트릭 서치?
 
 """
-
 def solution(s):
-    answer = 1 # 1글자는 무조건 팰린드롬 이므로 기본값 1로 초기화
+    answer = 0
+    
     n = len(s)
     
+    for flag in [True, False]:
     
+        left = 0
+        right = 0
     
-    
-    # 좌우 대칭 검사이므로 중심 확장형 구조 사용할 수 있다.
-    # 중심 확장형은 투포인터 알고리즘의 파생형이므로 체크하는 메서드의 매개변수는 2개이다.
-    def check(left, right): 
-        
-        
-        while left >= 0 and right < n:
-            
-            if s[left] != s[right]: # 한번이라도 다르면 실패이다.
-                return right - left - 1
+        for i in range(n):
+
+            if flag:
+                left = i
+                right = i+1
             else:
-                left -= 1 # 왼쪽으로 한칸 이동
-                right += 1 # 오른쪽으로 한칸 이동
+                left = i
+                right = i
+            
+
+            while left >= 0 and right < n: # 문자열 범위 안쪽
+                
+                if s[left] == s[right]: # 대칭
+                    if right - left + 1 > answer:
+                        answer = right - left + 1 # 길이 기록
+                    # print(f'left = {s[left]} right = {s[right]}로 {right - left + 1}글자 대칭 answer = {answer}')
+
+                    left -= 1
+                    right += 1
+
+
+                else: # 이번부터 대칭이 아님
+
+                    break
         
-        return right - left - 1 # ‼️마지막 확장은 실패한 상태이므로 되돌려야한다.‼️
-        
-    for i in range(n):
-        
-        answer = max(answer, check(i, i)) # 글자가 홀수개일 때
-        answer = max(answer, check(i, i+1))
-    
 
     return answer
-
-
-
-
-
-
-
 
 

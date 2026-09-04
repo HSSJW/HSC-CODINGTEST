7b708d24e8830c74e1a87d8ba940e29a97eab9a8
Song JinWoo <132650844+HSSJW@users.noreply.github.com>
2026-09-04 11:06:01 +0900

    [level 3] Title: 징검다리 건너기, Time: 237.08 ms, Memory: 19.6 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/README.md
+++ 프로그래머스/3/64062. 징검다리 건너기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 징검다리 건너기 - 64062 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64062?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64062) 
 
 ### 성능 요약
 
-메모리: 20.4 MB, 시간: 61.03 ms
+메모리: 19.6 MB, 시간: 237.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 08월 16일 18:50:43
+2026년 09월 04일 11:05:57
 
 ### 문제 설명
 
--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.py
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.py
@@ -1,87 +1,77 @@
 """
 <힌트>
-- 의사 코드 -> 시뮬레이션?
-- 디딤돌은 고정되어 있고 한칸씩 이동 -> 배열? / 그래프?
-- 칸마다 숫자가 정해져있고 이후 밟을 때마다 감소 -> 배열
-
-
-
-<의사 코드>
-1. 디딤돌마다 숫자가 적혀 있으며 한번 밟을 때 마다 1씩 줄어든다.
-2. 디딤돌의 숫자가 0이면 그 다음칸으로 건너 뛰어야한다.
-    - 건너뛴 디딤돌 중 처음으로 등장하는 '0이 아닌 디딤돌'로 이동해야한다.
-
+- 규칙 -> dp는 아니다.
+    - 징검다리 일렬, 칸마다 숫자 -> 배열
+    - stones[i] == 0 이면 더이상 밟을 수 없다.
+    - 밟을 수 있는(stones[i] > 0) 디딤돌이 여러 개인 경우 무조건 가장 가까운 곳
 
 <조건>
-- 건너야 하는 사람 수 무제한
-- 디딤돌 개수 200,000 -> 2차원 배열 불가능 -> 완전탐색(일반 경로탐색) 불가능
-- 한명이 모두 건넌 후에야 다음 사람 시도 가능
-- k : 한번에 건너 뛸 수 있는 최대 개수
+stones : 숫자가 담긴 배열
+k : 한번에 건널 수 있는 최대 칸수
+
 
 <목표>
-- 최대 몇명 까지 건널 수 있는지 => 중간에 끊기는 첫 구간 등장 전까지
+- 최대 몇명까지 건널 수 있는지? -> 완전 탐색
+    -> 200,000개 원소이므로 2차원 배열 불가능하다.
+    
+<접근>
+- k개 구간의 숫자가 모두 
+    - 앞, 뒤 숫자보다 작은 경우를 찾는다.
+        -> 고정된 너비의 구간을 탐색한다.
+        -> 처음부터 끝까지 훑으면서 최저값을 기록한다.
 
-<접근 2>
-- stones[i]가 있을 때 stones[i-1] ~ stones[i-k] 까지 모두 stones[i]보다 작은 것
-    - 중에서 stones[i-1] >= stones[i-2] ... >= stones[i-k] 인 것
+stones[a]를 지났을 때 k개 연속으로 stones[a]보다 작으면 실패
+    - stones[a+1] stones[a+2] ... stones[a+k]
 
-<방법>
-dp[i] : stones[i-1] ~ stones[i-k] 중 max
-
-1. stones[i] 보다 stones[i-k] 가 작으면서
-2. stones[i-k] >= stones[i-k+1:i] 일 때
-
-여기서 기억해야하는 값은? -> 
-dp[i] : 이 칸까지 올 수 있는 수 : stones[i-k:i] 중에서 가장 큰 수
-
-    - max(stones[i-k:i]) <= stones[i]인 경우 -> dp[i] = max(dp[i-k:i])
-    -                       >                 -> dp[i] = stones[i]
+<접근3>
+- 모든 인덱스에 대해서 stones[i:k]에 대해 
 
 
-dp[i] : max(dp[i-k:i]) 와 stones[i] 중에 더 작은거
-
+<접근4>
+- 구해야되는 대상 -> '최대 몇명이 건널 수 있나?' -> dp, 경로탐색이 아닌  최대/최소 몇명(몇개)? -> 파라메트릭 서치?
+- 파라메트릭 서치 이려면 -> 구하는 대상이 k명에서 가능하면 k이하는 검사 할 필요없이 가능하다.
+    -> 파라메트릭 서치 -> lo, hi가 구하는 대상이되고 -> 해당 값은 범위를 지문에서 찾는다. 
+        -> check를 만족하면 목표쪽(최대 최소)으로 확장한다.
 """
 
-from collections import deque
 def solution(stones, k):
     answer = 0
-    n = len(stones)
-    
-    dp = [0] * n
-    max_dq = deque([])
-    
-    # 만약 앞에 i-k부터 i-1 까지 모두 자기보다 작으면 중단?
-    
-    for idx in range(k): # 0 ~ n 까지 미리 초기화
-        dp[idx] = stones[idx]
-        
-        while max_dq and dp[max_dq[-1]] <= dp[idx]: # 가장 오른쪽 값보다 크다 -> 내림차순 깨짐
-            max_dq.pop() # 제거
-        
-        max_dq.append(idx)    
-
-    
-    for i in range(k, n):
-        
-        if max_dq[0] < i - k: # 단조덱의 가장 왼쪽 값이 `윈도우 시작 인덱스`보다 작다면
-            max_dq.popleft() # 범위를 벗어났으므로 제거
             
-        # 지금 필요한건 i보다 앞쪽 k개의 디딤돌 dp값
-        tmp_max = dp[max_dq[0]] # 내림차순 단조덱읜 [0]번 -> 최댓값
+    lo, hi = 1, max(stones) # 1인 돌이 k연속 있거나, 최대값으로 모두 이루어진 경우
+    
+    
+    # mid명이 건너는게 가능한지 -> stones[i] <= mid 인 돌이 k연속 있는지
+    def check(mid):
         
-        dp[i] = min(tmp_max, stones[i])
-
-        # dp[i]의 값을 기준으로 인덱스를 덱에 넣어야 하므로 dp[i]의 값이 확정된 후에 덱에 넣기
-        # 단조 덱에 push하기 직전에는 반드시  while문으로 정렬 검사
-        while max_dq and dp[max_dq[-1]] <= dp[i]:
-            max_dq.pop()
+        count = 0
         
-        max_dq.append(i)
-        
-    answer = max(dp[n-k:n])
+        for stone in stones:
             
+            if stone < mid:
+                count += 1
+                
+                if count >= k:
+                    return False
+            else: # 연속이 깨졌으므로 초기화
+                count = 0
+            
+        return True
+    
+    #---------------------------------------------
+    while lo <= hi:
+        
+        mid = (lo + hi) // 2
+        
+        if check(mid):
+            answer = mid # 최소 mid만큼은 건널 수 있다.
+            lo = mid + 1 # 최대값을 구해야 하므로 더 작은 쪽은 버린다.
+        
+        else:
+            hi = mid - 1 # k명 이상은 불가능하다.
+        
+        
+    
     return answer
-
 
 
 

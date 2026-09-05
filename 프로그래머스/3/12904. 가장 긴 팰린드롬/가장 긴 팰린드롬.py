0239150ed88b0fd55ca989013a1fcceffba84cf2
Song JinWoo <132650844+HSSJW@users.noreply.github.com>
2026-09-05 19:16:14 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 182.93 ms, Memory: 11.1 MB -BaekjoonHub

--- 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.py
+++ 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.py
@@ -1,47 +1,57 @@
-"""
-<힌트>
-- 팰린드롬 : 앞뒤를 뒤집어도 똑같은 문자열
-- s의 부분문자열 중에서 가장 긴 팰린드롬의 길이
-    -> 대칭성 확인
-
-
-"""
 def solution(s):
-    answer = 0
-    
+    answer = 0 # 더 큰 팰린드롬이 발견되면 초기화
     n = len(s)
     
-    for flag in [True, False]:
+    # 고정된 데이터에 대해 `구간`에 대한 검사 -> 투포인터
+    # 좌우 대칭 검사 -> 중앙 확장형 투포인터
+    # 투포인터로 대칭검사 할 때는 i에서 a, b모두 출발하는 경우와
+    # a : i b : i+1인 경우를 모두 수행해야한다.
+    # 투포인터는 고정된 데이터의 `모든 구간`에 대해서 수행한다.
+    # 구간의 너비는 right - left + 1
     
-        left = 0
-        right = 0
-    
-        for i in range(n):
-
-            if flag:
-                left = i
-                right = i+1
-            else:
-                left = i
-                right = i
-            
-
-            while left >= 0 and right < n: # 문자열 범위 안쪽
-                
-                if s[left] == s[right]: # 대칭
-                    if right - left + 1 > answer:
-                        answer = right - left + 1 # 길이 기록
-                    # print(f'left = {s[left]} right = {s[right]}로 {right - left + 1}글자 대칭 answer = {answer}')
-
-                    left -= 1
-                    right += 1
-
-
-                else: # 이번부터 대칭이 아님
-
-                    break
+    for i in range(n):
+        # 한점에서 출발
+        left = i
+        right = i
         
+        count = 0
+        while left >= 0 and right < n: #구간을 벗어나지 않는 동안
+            
+            if s[left] == s[right]: # 대칭 성립
+                
+                count = right - left + 1
+                
+                left -= 1
+                right += 1
+                
+                
+            else: # 대칭 깨짐
+                break
+                
+        if answer < count:
+            answer = count
+    
+    # 중앙 확장형 대칭검상에서는 i i+1에서 출발하는 것도 따로 검사한다.
+    
+
+    for k in range(n-1):
+
+        # 한점에서 출발
+        left = k
+        right = k+1
+        
+        count = 0
+        while left >= 0 and right < n: #구간을 벗어나지 않는 동안
+            
+            if s[left] == s[right]: # 대칭 성립
+                count = right - left + 1
+
+                left -= 1
+                right += 1
+            else: # 대칭 깨짐
+                break
+
+        if answer < count:
+            answer = count
 
     return answer
-
-
